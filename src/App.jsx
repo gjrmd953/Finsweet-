c72b5b62a6e3496ad88b735e11c367faf1d1daef
gjrmd953 <gjrmd953@gmail.com>
2025-05-19 10:50:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Banner from "./components/banner/Banner"
+import Header from "./components/header/Header"
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Banner />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./header.css";
+import Log from "../../assets/logo.png"
+
+const Header = () => {
+  return (
+    <div id='menubar'>
+        <div className="container">
+          <div className="main">
+             <div className="logo">
+            <img src={Log} alt="" />
+          </div>
+          <div className="menu">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Sermon</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+          </div>
+          <div className="btn">
+            <a href="#">Contact us</a>
+          </div>
+          </div>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
