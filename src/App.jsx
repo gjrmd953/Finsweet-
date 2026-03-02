3c9003d8c5573864a666e93810173199b1bc8064
gjrmd953 <gjrmd953@gmail.com>
2026-03-02 23:31:47 +0600

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Banner from "./components/banner/Banner"
+import Blog from "./components/blog/Blog"
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 
@@ -7,6 +8,7 @@ function App() {
     <>
     <Header />
     <Banner />
+    <Blog/>
     <Footer/>
     </>
   )
