944faf2b395367b73596866f00e66928fc760c26
gjrmd953 <gjrmd953@gmail.com>
2026-03-02 23:36:53 +0600

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Banner from "./components/banner/Banner"
 import Blog from "./components/blog/Blog"
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
+import Join from "./components/join/Join"
 import We from "./components/we/We"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
     <Header />
     <Banner />
+    <Join/>
     <We/>
     <Blog/>
     <Footer/>
